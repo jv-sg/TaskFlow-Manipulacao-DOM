476437da78426d536110b0cadbdfaddf4b13cd00
João Victor <jvictor.sg@outlook.com>
2026-03-24 17:08:51 -0300

    Adicionado a lógica basica de criação de cards e movimento

--- TaskFlow-Manipulacao-DOM/JS/main.js
+++ TaskFlow-Manipulacao-DOM/JS/main.js
@@ -19,13 +19,45 @@ const dragsEnter = ({ target }) => {
 }
 
 const dragsLeave = ({ target }) => {
-        target.classList.remove("column--highlight")
+    target.classList.remove("column--highlight")
+}
+
+const drop = ({ target }) => {
+    if (target.classList.contains("column__cards")){
+    target.classList.remove("column--highlight")
+    target.append(draggedCard)
+    }
+}
+
+const createCard = (event) => {
+    const target = event.target;
+    if (target.classList.contains("card")) {
+        target.contentEditable = "true";
+        target.focus();
+        return;
+    }
+
+    if (target.classList.contains("column__cards")){
+        const card = document.createElement("section")
+        card.className = "card"
+        card.draggable = "true"
+        card.contentEditable = "true"
+        card.addEventListener("focusout", () => {
+            card.contentEditable = "false"
+            if (!card.textContent) card.remove()
+        })
+        card.addEventListener("dragstart", dragStart)
+        target.append(card)
+        card.focus()
+    }
 }
 
 columns.forEach((column) => {
     column.addEventListener("dragover", dragsOver)
     column.addEventListener("dragenter", dragsEnter)
     column.addEventListener("dragleave", dragsLeave)
+    column.addEventListener("drop", drop)
+    column.addEventListener("dblclick", createCard)
 })
 
 cards.forEach((card) => {
